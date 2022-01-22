30dbea047e4f806c5fc4582f2b9109d1c37dd73d
Hongcheng-Shen <49156279+Hongcheng-Shen@users.noreply.github.com>
2022-01-22 14:27:12 +0800

    Add files via upload
    修改人体感应方块显示

--- Quadruped.ts
+++ Quadruped.ts
@@ -332,7 +332,7 @@ namespace Quadruped {
     */
     //% subcategory=sensor
     //% blockGap=8
-    //% blockId=sensor_Human_Infrared block="Human Infrared|pin|%pin|mode| %value"
+    //% blockId=sensor_Human_Infrared block="Human Infrared|pin|%pin"
     export function Human_induction(pin: AnalogPin, value=50): number {
         let w = pins.analogReadPin(pin)
         if (w >= value)
